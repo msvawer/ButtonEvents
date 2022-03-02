d12842f96939175a52879bacf6e2f84a47d38180
MichVawer <michelle.vawer@icloud.com>
2022-03-02 14:02:47 -0500

    SceneInfo class and SceneMenu editor extension class created and added

---
+++ Assets/Scripts/SceneInfo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneInfo 
+{
+    public static class Names
+    {
+        public static readonly string XRPersistent = "XRPersistent";
+        public static readonly string CowboySpaceship = "CowboyUp";
+        public static readonly string GridRoom = "GridRoom";
+        public static readonly string Lobby = "Lobby";
+
+    }
+
+
+       
+}
